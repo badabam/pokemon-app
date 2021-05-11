a62b6a9ca8eca2a4a8bb44c47f2b3f60e4ca6e6c
Jeremias Erbs <jerry@badabam.de>
2021-05-11 13:02:09 +0200

    Improve Card, add types

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import Card from './Card'
 import pokemons from './data.json'
 
 export default function App() {
-  const firstPokemon = pokemons[0]
-
+  const { name, images, types } = pokemons[0]
   return (
     <div className="App">
-      <Card name={firstPokemon.name} frontImage={firstPokemon.images.front} />
+      <Card name={name} images={images} types={types} />
     </div>
   )
 }
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,17 @@
 import './Card.css'
 
-export default function Card({ name, frontImage }) {
+export default function Card({ name, images, types }) {
+  const [type1, type2] = types
+
   return (
     <section className="Card">
       <h2>{name}</h2>
-      <img src={frontImage} alt="" />
+      <ul>
+        <li>{type1}</li>
+        <li>{type2}</li>
+      </ul>
+      <img src={images.front} alt="" />
+      <img src={images.back} alt="" />
     </section>
   )
 }
